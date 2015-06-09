c9d87174650de2eee564221300e9ab5e5f02a987
katetikhomirova <romas4ka94@gmail.com>
2015-06-09 12:12:48 +0300

    Add service project

---
+++ CyclingWorld-Service/src/main/java/ua/nure/tikhomirova/rest/GreetingController.java
@@ -0,0 +1,14 @@
+package ua.nure.tikhomirova.rest;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+class GreetingController {
+    
+    @RequestMapping("/hello/{name}")
+    String hello(@PathVariable String name) {
+        return "Hello, " + name + "!";
+    }
+}
